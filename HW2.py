7aafb50e871515b0335e37c5835a50fb7828fa09
nightted <h5904098@gmail.com>
2020-06-24 23:33:10 +0800

    add

---
+++ HW2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 13:09:43 2020
+
+@author: h5904
+"""
+import numpy as np
+
+np.random.seed(0)
+X_train_fpath = './data/X_train'
+Y_train_fpath = './data/Y_train'
+X_test_fpath = './data/X_test'
+output_fpath = './output_{}.csv'
+
+# Parse csv files to numpy array
+with open(X_train_fpath) as f:
+    next(f)
+    X_train = np.array([line.strip('\n').split(',')[1:] for line in f], dtype = float)
+with open(Y_train_fpath) as f:
+    next(f)
+    Y_train = np.array([line.strip('\n').split(',')[1] for line in f], dtype = float)
+with open(X_test_fpath) as f:
+    next(f)
+    X_test = np.array([line.strip('\n').split(',')[1:] for line in f], dtype = float)
+    
+print(X_train)
